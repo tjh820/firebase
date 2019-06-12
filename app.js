ae2378ac4f553ae7488e677d7f25ce27235b868d
Tiffany Hollenbeck <tjh820@yahoo.com>
2019-06-12 19:09:13 -0400

    next train time displaying

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyB7_c5SFtFFwc52dm29WNCzfKdHCE9o9Q4",
-    authDomain: "crane-2bd76.firebaseapp.com",
-    databaseURL: "https://crane-2bd76.firebaseio.com",
-    projectId: "crane-2bd76",
-    storageBucket: "",
-    messagingSenderId: "993037148950",
-    appId: "1:993037148950:web:46aa12384bdd27b5"
-  };
+   apiKey: "AIzaSyB7_c5SFtFFwc52dm29WNCzfKdHCE9o9Q4",
+   authDomain: "crane-2bd76.firebaseapp.com",
+   databaseURL: "https://crane-2bd76.firebaseio.com",
+   projectId: "crane-2bd76",
+   storageBucket: "",
+   messagingSenderId: "993037148950",
+   appId: "1:993037148950:web:46aa12384bdd27b5"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
 
-  $("button").on("click",function(){
-     var newTrain = {
-         name: $("#train-name").val().trim(),
-        first: $("#first-train").val().trim(),
-        destination: $("#destination").val().trim(),
-        frequency: $("#frequency").val().trim(),
-    
-        
-        
-     }
-     database.ref().push(newTrain)
-     console.log(newTrain);
+$("button").on("click", function () {
+   var newTrain = {
+      name: $("#train-name").val().trim(),
+      firstTrain: $("#first-train").val().trim(),
+      destination: $("#destination").val().trim(),
+      frequency: $("#frequency").val().trim(),
 
-     var startHour = moment();
- 
 
-     console.log(startHour);
-     
 
-     var trainData = $("<tr>").append("<td>" + newTrain.name + "</td>")
-     trainData.append("<td>" + newTrain.first + "</td>")
-     trainData.append("<td>" + newTrain.destination + "</td>")
-     trainData.append("<td>" + newTrain.frequency + "</td>")
+   }
+   database.ref().push(newTrain)
+   console.log(newTrain);
+   var currentDate = moment().format("MM/DD/YYYY")
 
-     $("#trains").append(trainData);
+   var startTime = moment(currentDate + " " + newTrain.firstTrain)
+   console.log(startTime)
+   var trainTime = startTime
 
+   while (trainTime.isBefore(moment())){
+      trainTime.add(newTrain.frequency, "minutes")
+   }
    
-  });
+
+
+
+
+
+
+   var trainData = $("<tr>").append("<td>" + newTrain.name + "</td>")
+   trainData.append("<td>" + newTrain.destination + "</td>")
+   trainData.append("<td>" + newTrain.frequency + "</td>")
+   trainData.append("<td>" + trainTime.format("hh:mm") + "</td>")
+
+   $("#trains").append(trainData);
+
+
+});
