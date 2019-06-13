944577be4b6e49d89f85059877a42282a7f7759e
Tiffany Hollenbeck <tjh820@yahoo.com>
2019-06-13 14:30:15 -0400

    final final final results

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 
-
+//on click listener
 $("button").on("click", function () {
    var newTrain = {
       name: $("#train-name").val().trim(),
@@ -22,28 +22,29 @@ $("button").on("click", function () {
 
 
    }
+   //pushing data to db
    database.ref().push(newTrain)
-   console.log(newTrain);
+   //get current date in use for creating the time
    var currentDate = moment().format("MM/DD/YYYY")
-
    var startTime = moment(currentDate + " " + newTrain.firstTrain)
-   console.log(startTime)
-   var trainTime = startTime
-
-   while (trainTime.isBefore(moment())){
-      trainTime.add(newTrain.frequency, "minutes")
+   
+//adds freq to startTime until u reach the current time
+   while (startTime.isBefore(moment())){
+      startTime.add(newTrain.frequency, "minutes")
    }
    
+//train time from now
+ var newTime = startTime.diff(moment(), "minutes");
 
 
 
-
-
+//formatting data & appending it to table
 
    var trainData = $("<tr>").append("<td>" + newTrain.name + "</td>")
    trainData.append("<td>" + newTrain.destination + "</td>")
    trainData.append("<td>" + newTrain.frequency + "</td>")
-   trainData.append("<td>" + trainTime.format("hh:mm") + "</td>")
+   trainData.append("<td>" + startTime.format("hh:mm a") + "</td>")
+   trainData.append("<td>" + newTime + "</td>")
 
    $("#trains").append(trainData);
 
